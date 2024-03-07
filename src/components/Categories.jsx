243a1a4db91863f2ef1dab4b54797421516e76d6
Mercede Chen <mercedechen@gmail.com>
2024-03-06 22:45:39 -0500

    Add functionality for the remaining category filters

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,60 @@
 function Categories({ data, setFilteredProducts }) {
+  // Removes category filter by setting setFilterProducts in Products component back to an empty array and displays all products from database.
+  const removeFilter = () => {
+    setFilteredProducts([]);
+  }
 
-  const filterProducts = () => { 
-    const filterByElectronics = data.filter((product) => {
+  // Displays products with category: men's clothing
+  const filterByMen = () => {
+    const men = data.filter((product) => {
+      return (
+        product.category.toLowerCase().includes(`men's clothing`)
+      )
+    });
+    setFilteredProducts(men);
+    console.log(`men's clothing`, men)
+  }
+
+  // Displays products with category: women's clothing
+  const filterByWomen = () => {
+    const women = data.filter((product) => {
+      return (
+        product.category.toLowerCase().includes(`women's clothing`)
+      )
+    });
+    setFilteredProducts(women);
+    console.log(`women's clothing`, women);
+  }
+
+  // Displays products with category: jewelery
+  const filterByJewelery = () => {
+    const jewelery = data.filter((product) => {
+      return (
+        product.category.toLowerCase().includes("jewelery")
+      )
+    });
+    setFilteredProducts(jewelery);
+    console.log('jewelery', jewelery);
+  }
+
+  // Displays products with category: electronics
+  const filterByElectronics = () => { 
+    const electronics = data.filter((product) => {
       return (
         product.category.toLowerCase().includes("electronics")
-      )
-    })
-    setFilteredProducts(filterByElectronics);
-    console.log('electronics', filterByElectronics);
+        )
+      });
+      setFilteredProducts(electronics);
+      console.log('electronics', electronics);
   }
 
   return (
     <div className="filter">
-      <button onClick={filterProducts}>Electronics</button>
+      <button onClick={removeFilter}>All</button>
+      <button onClick={filterByMen}>Men</button>
+      <button onClick={filterByWomen}>Women</button>
+      <button onClick={filterByJewelery}>Jewelery</button>
+      <button onClick={filterByElectronics}>Electronics</button>
     </div>
   )
 }
